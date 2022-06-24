066d5038d5e426fe12f6c7dc8c2734c98a6aa04c
Burak Oz <oz.burak1995@gmail.com>
2022-06-25 01:58:29 +0200

    initial commit

---
+++ contracts/ETHBBSEPriceFeedOracle.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ETHBBSEPriceFeedOracle is Ownable {
+  // Max number of blocks before a price update is required
+  uint8 public constant MAX_PRICE_AGE = 3;
+
+  // ETH/BBSE rate
+  uint private rate;
+
+  // Block number of the last rate update.
+  uint public lastUpdateBlock;
+
+  // An event that indicates the price of the priceFeed should be updated
+  // Must be listened by the oracle server
+  event GetNewRate (string priceFeed);
+  
+  /**
+  * @dev Initializes lastUpdateBlock to current block number and rate to 0.
+  * Emits GetNewRate to trigger the oracle server to update the rate.
+  * The priceFeed parameter of GetNewRate should be ETH/BBSE
+  */
+  constructor () public { 
+    // TODO: Complete the constructor
+  }
+
+  /**
+  * @dev Updates the rate and sets the lastUpdateBlock to current block number.
+  * Can only be called by the owner of the oracle contract.
+  * Can't be called internally.
+  * @param _rate new rate of the price feed.
+  */
+  // TODO: Implement the updateRate function (use the respective function modifier from Ownable)
+
+  /**
+  * @dev Returns the current rate.
+  * If rate was updated more than MAX_PRICE_AGE block ago,
+  * emits GetNewRate event to trigger the oracle server.
+  */
+  // TODO: Implement the getRate function
+}
